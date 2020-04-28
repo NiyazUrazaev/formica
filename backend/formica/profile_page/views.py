2e9934661240367b763b4e0ccb5c62906051b303
NiyazUrazaev <niyazurazaev@gmail.com>
2020-04-29 01:48:35 +0300

    Create api for looking profile page info

--- backend/formica/profile_page/views.py
+++ backend/formica/profile_page/views.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from django.contrib import auth
 
@@ -5,3 +6,23 @@ from django.contrib import auth
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .models import Profile
+
+
+class ProfilePageInfoView(APIView):
+
+    def get(self, request):
+
+        profile_id = request.GET.get('id', None)
+        if profile_id is None:
+            return Response(status=400, data='No profile_id in kwargs!')
+
+        try:
+            profile = Profile.objects.get(id=profile_id)
+        except Profile.DoesNotExist:
+            return Response(status=400, data='No profile with this id!')
+
+        return Response(status=200, data=model_to_dict(profile))
+
+    def post(self, request):
+        pass
